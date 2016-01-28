7959e0c51e76f0dc93357ae78eb6293f935afdce
Brian Croxall <brian.croxall@brown.edu>
2016-01-28 16:17:46 -0500

    First commit of visualizations page

---
+++ visualizations/index.php
@@ -0,0 +1,18 @@
+<?php
+	$selected = "visualizations";
+	require '../header.php';
+?>
+
+<ul id="breadcrumbs">
+	<li><a href="../">Main</a></li>
+	<li>Visualizations</li>
+</ul>
+
+<h2>Visualizations of the <em>Decameron</em></h2>
+<p>Coming soon.</p>
+
+
+<?php
+	$last_modified = filemtime($_SERVER["SCRIPT_FILENAME"]);
+	include '../footer.php';
+?>
