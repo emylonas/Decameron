3ed038be07581d2798b370e206af28872d306ce5
tnarla <trushithanarla@gmail.com>
2016-02-24 11:20:37 -0500

    Added hover and selected color changes to the side bar text

--- header.php
+++ header.php
@@ -77,24 +77,23 @@ header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Dispo
 	</div>
 	<div id="sidebar">
 		<ul>
-			<li id="the_project"><a href="<?php echo $relativePath?>the_project/"<?php if ($selected == "the_project") print(" class=\"selected\"") ?>>The Project</a></li>
 			<!-- Navigation -->
-			<!-- <li id="the_project"><a href="<?php echo $relativePath?>the_project/"<?php if ($selected == "the_project") print(" class=\"selected\"") ?>>The Project</a></li>
-			<li id="boccaccio"><a href="<?php echo $relativePath?>boccaccio/life1_en.php"<?php if ($selected == "boccaccio") print(" class=\"selected\"") ?>>Boccaccio</a></li>
-			<li id="texts"><a href="<?php echo $relativePath?>texts/"<?php if ($selected == "texts") print(" class=\"selected\"") ?>>Texts</a></li>
-			<li id="brigata"><a href="<?php echo $relativePath?>brigata/"<?php if ($selected == "brigata") print(" class=\"selected\"") ?>>Brigata</a></li>
-			<li id="plague"><a href="<?php echo $relativePath?>plague/"<?php if ($selected == "plague") print(" class=\"selected\"") ?>>Plague</a></li>
-			<li id="literature"><a href="<?php echo $relativePath?>literature/"<?php if ($selected == "literature") print(" class=\"selected\"") ?>>Literature</a></li>
-			<li id="history"><a href="<?php echo $relativePath?>history/"<?php if ($selected == "history") print(" class=\"selected\"") ?>>History</a></li>
-			<li id="society"><a href="<?php echo $relativePath?>society/"<?php if ($selected == "society") print(" class=\"selected\"") ?>>Society</a></li>
-			<li id="religion"><a href="<?php echo $relativePath?>religion/"<?php if ($selected == "religion") print(" class=\"selected\"") ?>>Religion</a></li>
-			<li id="arts"><a href="<?php echo $relativePath?>arts/"<?php if ($selected == "arts") print(" class=\"selected\"") ?>>Arts</a></li>
-			<li id="maps"><a href="<?php echo $relativePath?>images/maps/maps.php"<?php if ($selected == "maps") print(" class=\"selected\"") ?>>Maps</a></li>
-			<li id="themes_motifs"><a href="<?php echo $relativePath?>themes_motifs/"<?php if ($selected == "themes_motifs") print(" class=\"selected\"") ?>>Themes/Motifs</a></li>
-			<li id="bibliography"><a href="<?php echo $relativePath?>bibliography/"<?php if ($selected == "bibliography") print(" class=\"selected\"") ?>>Bibliography</a></li>
-			<li id="pedagogy"><a href="<?php echo $relativePath?>pedagogy/"<?php if ($selected == "pedagogy") print(" class=\"selected\"") ?>>Pedagogy</a></li>
-			<li id="syllabus"><a href="<?php echo $relativePath?>the_project/syllabus.php"<?php if ($selected == "syllabus") print(" class=\"selected\"") ?>>Syllabus</a></li>
-			<li id="visualizations"><a href="<?php echo $relativePath?>visualizations/"<?php if ($selected == "visualizations") print(" class=\"selected\"") ?>>Visualizations (new!)</a></li> -->
+			<li class = "side" id="the_project"><a href="<?php echo $relativePath?>the_project/"<?php if ($selected == "the_project") print(" class=\"selected\"") ?>>The Project</a></li>
+			<li class = "side" id="boccaccio"><a href="<?php echo $relativePath?>boccaccio/life1_en.php"<?php if ($selected == "boccaccio") print(" class=\"selected\"") ?>>Boccaccio</a></li>
+			<li class = "side" id="texts"><a href="<?php echo $relativePath?>texts/"<?php if ($selected == "texts") print(" class=\"selected\"") ?>>Texts</a></li>
+			<li class = "side" id="brigata"><a href="<?php echo $relativePath?>brigata/"<?php if ($selected == "brigata") print(" class=\"selected\"") ?>>Brigata</a></li>
+			<li class = "side" id="plague"><a href="<?php echo $relativePath?>plague/"<?php if ($selected == "plague") print(" class=\"selected\"") ?>>Plague</a></li>
+			<li class = "side" id="literature"><a href="<?php echo $relativePath?>literature/"<?php if ($selected == "literature") print(" class=\"selected\"") ?>>Literature</a></li>
+			<li class = "side" id="history"><a href="<?php echo $relativePath?>history/"<?php if ($selected == "history") print(" class=\"selected\"") ?>>History</a></li>
+			<li class = "side" id="society"><a href="<?php echo $relativePath?>society/"<?php if ($selected == "society") print(" class=\"selected\"") ?>>Society</a></li>
+			<li class = "side" id="religion"><a href="<?php echo $relativePath?>religion/"<?php if ($selected == "religion") print(" class=\"selected\"") ?>>Religion</a></li>
+			<li class = "side" id="arts"><a href="<?php echo $relativePath?>arts/"<?php if ($selected == "arts") print(" class=\"selected\"") ?>>Arts</a></li>
+			<li class = "side" id="maps"><a href="<?php echo $relativePath?>images/maps/maps.php"<?php if ($selected == "maps") print(" class=\"selected\"") ?>>Maps</a></li>
+			<li class = "side" id="themes_motifs"><a href="<?php echo $relativePath?>themes_motifs/"<?php if ($selected == "themes_motifs") print(" class=\"selected\"") ?>>Themes/Motifs</a></li>
+			<li class = "side" id="bibliography"><a href="<?php echo $relativePath?>bibliography/"<?php if ($selected == "bibliography") print(" class=\"selected\"") ?>>Bibliography</a></li>
+			<li class = "side" id="pedagogy"><a href="<?php echo $relativePath?>pedagogy/"<?php if ($selected == "pedagogy") print(" class=\"selected\"") ?>>Pedagogy</a></li>
+			<li class = "side" id="syllabus"><a href="<?php echo $relativePath?>the_project/syllabus.php"<?php if ($selected == "syllabus") print(" class=\"selected\"") ?>>Syllabus</a></li>
+			<li class = "side" id="visualizations"><a href="<?php echo $relativePath?>visualizations/"<?php if ($selected == "visualizations") print(" class=\"selected\"") ?>>Visualizations</a></li>
 			<!-- End -->
 		</ul>
 
